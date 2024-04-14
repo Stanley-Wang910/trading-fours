0c41bb0b4929c83e06dded850adef6e1f7891e42
Stanley Wang <wangstanley910@gmail.com>
2024-04-14 16:49:00 -0400

    margin change logout cmpnt

--- react_app/src/components/Logout.js
+++ react_app/src/components/Logout.js
@@ -13,7 +13,7 @@ function Logout({ setToken, setRecommendations }) {
 
   return (
     <button
-      className="absolute top-4 right-4 px-4 py-2 bg-blue-700 text-gray-200 rounded-full hover:bg-blue-600 transition-colors duration-300"
+      className="absolute top-4 right-0 px-4 py-2 bg-blue-700 text-gray-200 rounded-full hover:bg-blue-600 transition-colors duration-300"
       onClick={handleLogout}>
       Logout
     </button>
